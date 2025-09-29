3f6b0f1d327ac08779a73a43045199bfcaa2d115
ZoeSonrisaM <152182342+ZoeSonrisaM@users.noreply.github.com>
2025-09-29 16:20:39 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 let button = document.querySelector("header button");
 let nav = document.querySelector("header nav");
+let body = document.body; // deze is nieuw
 
 function toggleMenu() {
   nav.classList.toggle("open");
+  body.classList.toggle("scroll-lock"); // deze is nieuw
 }
 
 button.onclick = toggleMenu;
